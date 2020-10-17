b8b0503a0c44fea4caba9265fcf1cc9770f81488
Andrew Lisowski <lisowski54@gmail.com>
2020-10-16 17:44:49 -0700

    add gallery to doc-blocks package

--- packages/doc-blocks/src/index.tsx
+++ packages/doc-blocks/src/index.tsx
@@ -2,6 +2,7 @@ export * from '@doc-blocks/accessibility';
 export * from '@doc-blocks/bundle-size';
 export * from '@doc-blocks/design-spec';
 export * from '@doc-blocks/guideline';
+export * from '@doc-blocks/gallery';
 export * from '@doc-blocks/intended-usage';
 export * from '@doc-blocks/related-components';
 export * from '@doc-blocks/responsive-story';
